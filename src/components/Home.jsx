a161d66f5a66ab884b6b4da1df75aca067bfa1e3
Wasim Hyder <wasim@creativefaze.com>
2023-03-09 16:44:28 +0500

    Completed home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,13 @@
 export default function Home() {
   return(
-    <div className="home">
-      <h1>This is home</h1>
+    <div className="home-banner">
+      <div className="content">
+        <h3 className="plans">You got the travel plans, we got the travel vans.</h3>
+        <div className="info">
+          <p>Add adventure to your life by joining the #vanlife movement. Rent the perfect van to make your perfect road trip.</p>
+          <button>Find your van</button>
+        </div>
+      </div>
     </div>
   )
 }
