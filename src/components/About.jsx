12dba65bc2daf85201e91757a03611a985ddc767
Wasim Hyder <wasim@creativefaze.com>
2023-03-10 16:34:39 +0500

    Debugging

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,7 @@ export default function About() {
     <div className="about-page">
       <div className="about-banner"></div>
       <div className="about-content">
-        <h3>Don’t squeeze in a sedan when you could relax in a van.</h3>
+        <h3>ddDon’t squeeze in a sedan when you could relax in a van.</h3>
         <p>Our mission is to enliven your road trip with the perfect travel van rental. Our vans are recertified before each trip to ensure your travel plans can go off without a hitch. (Hitch costs extra 😉) <br /><br />
         Our team is full of vanlife enthusiasts who know firsthand the magic of touring the world on 4 wheels.</p>
         <div className="explore-vans">
