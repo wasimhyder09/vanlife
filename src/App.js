941addcc4e95c479d190cee3177c200cedc8f283
Wasim Hyder <wasim@creativefaze.com>
2023-03-10 17:39:49 +0500

    Updated directory structure

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import Nav from './components/Nav';
-import Footer from './components/Footer';
+import Nav from './pages/Nav';
+import Footer from './pages/Footer';
 
 function App() {
   return (
