d46c98fcb22f7e2331cd557291d6f6da12d1e1f3
Wasim Hyder <wasim@creativefaze.com>
2023-03-11 21:55:35 +0500

    Created host vans listing page

--- src/pages/Host/HostVans.jsx
+++ src/pages/Host/HostVans.jsx
@@ -1,5 +1,32 @@
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 export default function HostVans() {
+  const [hostVans, setHostVans] = useState([])
+
+  useEffect(() => {
+    fetch("/api/host/vans")
+    .then(res => res.json())
+    .then(data => setHostVans(data.vans))
+  }, [])
+
+  const vanElement = hostVans.map(van => (
+    <Link key={van.id} to={`/host/vans/${van.id}`}>
+      <div className="van-host-tile">
+        <div className="left-side">
+          <img src={van.imageUrl} alt="Van" />
+        </div>
+        <div className="right-side">
+          <h3>{van.name}</h3>
+          <p>${van.price}/day</p>
+        </div>
+      </div>
+    </Link>
+  ))
+
   return(
-    <h1>This is host vans page</h1>
+    <div className="listed-host-vans">
+      <h1>Your listed vans</h1>
+      { vanElement }
+    </div>
   )
 }
