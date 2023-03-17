59e6235e94eb38f1212950ac545fca39819a98da
Wasim Hyder <wasim@creativefaze.com>
2023-03-17 17:47:08 +0500

    Implemented deffered data state for host vans

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -9,7 +9,7 @@ import Dashboard from "../pages/Host/Dashboard";
 import Income from "../pages/Host/Income";
 import Reviews from "../pages/Host/Reviews";
 import HostLayout from "./HostLayout";
-import HostVans from "../pages/Host/HostVans";
+import HostVans, {loader as vansHostLoader} from "../pages/Host/HostVans";
 import HostVanDetail from "../pages/Host/HostVanDetail";
 import HostVanPricing from "../pages/Host/HostVanPricing";
 import HostVanPhotos from "../pages/Host/HostVanPhotos";
@@ -23,7 +23,7 @@ const router = createBrowserRouter(createRoutesFromElements(
   <Route path="/" element={<Layout />}>
     <Route index element={<Home />} />
     <Route path="about" element={<About />} />
-    <Route path="vans" element={<Vans />} loader={vansLoader}  errorElement={<Error />}/>
+    <Route path="vans" element={<Vans />} loader={vansLoader} errorElement={<Error />}/>
     <Route path="vans/:id" element={<VanDetail />} />
     <Route
       path="login"
@@ -34,7 +34,7 @@ const router = createBrowserRouter(createRoutesFromElements(
       <Route path="host" element={<HostLayout />} >
         <Route index element={<Dashboard />} />
         <Route path="income" element={<Income />} />
-        <Route path="vans" element={<HostVans />} />
+        <Route path="vans" element={<HostVans />} loader={vansHostLoader} errorElement={<Error />} />
         <Route path="vans/:id" element={<HostVanDetail />}>
           <Route index element={<HostVanInfo />} />
           <Route path="pricing" element={<HostVanPricing />} />
--- src/pages/Host/HostVans.jsx
+++ src/pages/Host/HostVans.jsx
@@ -1,32 +1,40 @@
-import { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import { Suspense } from "react";
+import { Link, defer, useLoaderData, Await } from "react-router-dom";
+import { getHostVans } from "../../api";
+import '../../server';
+
+export function loader() {
+  return defer({hostVans: getHostVans()})
+}
+
 export default function HostVans() {
-  const [hostVans, setHostVans] = useState([])
+  const dataPromise = useLoaderData()
 
-  useEffect(() => {
-    fetch("/api/host/vans")
-    .then(res => res.json())
-    .then(data => setHostVans(data.vans))
-  }, [])
-
-  const vanElement = hostVans.map(van => (
-    <Link key={van.id} to={van.id}>
-      <div className="van-host-tile">
-        <div className="left-side">
-          <img src={van.imageUrl} alt="Van" />
+  function renderHostVans(hostVans) {
+    const vanElement = hostVans.map(van => (
+      <Link key={van.id} to={van.id}>
+        <div className="van-host-tile">
+          <div className="left-side">
+            <img src={van.imageUrl} alt="Van" />
+          </div>
+          <div className="right-side">
+            <h3>{van.name}</h3>
+            <p>${van.price}/day</p>
+          </div>
         </div>
-        <div className="right-side">
-          <h3>{van.name}</h3>
-          <p>${van.price}/day</p>
-        </div>
-      </div>
-    </Link>
-  ))
+      </Link>
+    ))
+    return vanElement
+  }
 
   return(
     <div className="listed-host-vans">
       <h1>Your listed vans</h1>
-      { vanElement }
+      <Suspense fallback={<h2>Loading vans....</h2>}>
+        <Await resolve={dataPromise.hostVans}>
+          {renderHostVans}
+        </Await>
+      </Suspense>
     </div>
   )
 }
